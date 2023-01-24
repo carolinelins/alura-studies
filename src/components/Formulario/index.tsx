55353d41cb7d523c7bbae047e6f145fdcbd4f924
Caroline Lins <carolineglins@gmail.com>
2023-01-24 14:46:57 -0300

    Usando CSS Modules

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Botao from "../Botao";
-import './style.scss'
+import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
     render(): React.ReactNode {
         return (
-            <form className="novaTarefa">
-                <div className="inputContainer">
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
                     <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required />
                 </div>
-                <div className="inputContainer">
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
